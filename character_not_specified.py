60dd7d1db8ee62cf01b0a78898c398bd555735d1
CROW_07 <100528047+HuyusakiYasiti@users.noreply.github.com>
2022-10-13 23:47:08 +0900

    Add files via upload

---
+++ character_not_specified.py
@@ -0,0 +1,29 @@
+import re
+
+number = int()
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+chosen = str()
+flag = str()
+response = str()
+
+while True:
+    chosen = input("Please enter a letter. : ")
+    if chosen in letters: 
+        number = letters.index(chosen)
+        letters.remove(chosen)
+        letters.insert(number, " ")
+
+    flag = input("Please enter a letter to countinue. :")
+    flag = bool(flag)
+    if flag == True:
+        continue
+
+    break
+
+letters = str(letters)
+letters = letters.replace("\'", "")
+letters = letters[1: -1]
+
+print(letters)
